8f9c210fffcb69e688b0de1c6dbe350c92ba3576
bleads2 <bleads2@gmail.com>
2023-05-11 07:11:58 -0700

    Done App component and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,17 @@ import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { NopageComponent } from './nopage/nopage.component';
 import { FormsModule } from '@angular/forms';
 import { SelerHomeComponent } from './seler-home/seler-home.component';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SearchComponent } from './search/search.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { MyOrderComponent } from './my-order/my-order.component';
 
 @NgModule({
   declarations: [
@@ -17,13 +28,24 @@ import { SelerHomeComponent } from './seler-home/seler-home.component';
     HomeComponent,
     SellerAuthComponent,
     NopageComponent,
-    SelerHomeComponent
+    SelerHomeComponent,
+    SellerAddProductComponent,
+    SellerUpdateProductComponent,
+    SearchComponent,
+    ProductDetailsComponent,
+    UserAuthComponent,
+    FooterComponent,
+    CartPageComponent,
+    CheckoutComponent,
+    MyOrderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
