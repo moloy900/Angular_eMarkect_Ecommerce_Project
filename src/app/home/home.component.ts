1d051a30c9dac2d69d9e1a4ea89460f1cf078ca5
bleads2 <bleads2@gmail.com>
2023-05-11 06:27:58 -0700

    Done with my home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { cart, product } from 'src/data-type';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  productDetails: product | undefined;
+  productQualtity: number = 1;
+  proProducts: product[] | undefined;
+  trandyProducts: product[] | undefined;
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.propularProducts().subscribe((res) => {
+      console.log(res);
+
+      this.proProducts = res;
+    });
+    this.productService.trendyProducts().subscribe((res) => {
+      this.trandyProducts = res;
+    });
+  }
 }
