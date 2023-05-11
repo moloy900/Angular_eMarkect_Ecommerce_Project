aab59117765106a8b606106e4961dcfbbe4154ce
bleads2 <bleads2@gmail.com>
2023-05-11 07:10:29 -0700

    Done interface typescript file integrated

--- src/data-type.ts
+++ src/data-type.ts
@@ -4,8 +4,49 @@ export interface signUp {
   email: string;
 }
 
+export interface logIn {
+  email: string;
+  password: string;
+}
 
-export interface logIn{
-  email:string;
-  password:string;
+export interface product {
+  name: string;
+  price: number;
+  color: string;
+  category: string;
+  description: string;
+  image: string;
+  id: number;
+  quantity: number | undefined;
+  productId: undefined | number;
+}
+
+export interface cart {
+  name: string;
+  price: number;
+  color: string;
+  category: string;
+  description: string;
+  image: string;
+  id: number | undefined;
+  quantity: number | undefined;
+  userId: number;
+  productId: number;
+}
+
+export interface priceSummery {
+  price: number;
+  discount: number;
+  tax: number;
+  delivery: number;
+  total: number;
+}
+
+export interface order {
+  email: string;
+  address: string;
+  contact: string;
+  totalPrice: number;
+  userId: number;
+  id: number|undefined;
 }
